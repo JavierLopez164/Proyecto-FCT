f0bfb29cb275ca9828c96b1e645446027e3a706a
Alvaro Almodovar <alvaro.almodovar1@educa.madrid.org>
2025-05-24 02:58:11 +0200

    Hecho la galeria de comidas

--- cliente-web/src/main.ts
+++ cliente-web/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { LOCALE_ID } from '@angular/core';
+registerLocaleData(localeEs);
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    { provide: LOCALE_ID, useValue: 'es' }
+  ]
+}).catch(err => console.error(err));
